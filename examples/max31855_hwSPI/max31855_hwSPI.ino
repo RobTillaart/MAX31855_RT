951934208ecb691473123e0beba9a4a24fed6537
rob tillaart <rob.tillaart@gmail.com>
2021-07-04 17:06:33 +0200

    fix #14 CS for STM32

--- examples/max31855_hwSPI/max31855_hwSPI.ino
+++ examples/max31855_hwSPI/max31855_hwSPI.ino
@@ -1,13 +1,15 @@
 //
 //    FILE: max31855_hw_SPI.ino
 //  AUTHOR: Rob Tillaart
-// VERSION: 0.1.0
+// VERSION: 0.1.1
 // PURPOSE: thermocouple lib demo application
 //    DATE: 2020-08-30
 //     URL: https://github.com/RobTillaart/MAX31855_RT
 //
 
+
 #include "MAX31855.h"
+
 
 //
 // | HW SPI   |  UNO  |  ESP32  |
@@ -16,7 +18,7 @@
 // | MISO     |   12  |   19    |
 // | MOSI     |   11  |   23    |
 
-// read()            timing UNO 
+// read()            timing UNO
 // HWSPI  16000000   ~68  us
 // HWSPI   4000000   ~72  us
 // HWSPI   1000000   ~100 us
@@ -27,18 +29,22 @@ const int csPin = 6;
 
 uint32_t start, stop;
 
+
 MAX31855 tc(csPin);
 // MAX31855 tc(13, 6, 12);  // sw SPI
+
 
 void setup()
 {
   Serial.begin(115200);
+  Serial.println(__FILE__);
   Serial.print("Start max31855_demo0: ");
   Serial.println(MAX31855_VERSION);
   Serial.println();
 
   tc.begin();
 }
+
 
 void loop()
 {
@@ -55,7 +61,14 @@ void loop()
 
   uint32_t raw = tc.getRawData();
   Serial.print("raw:\t\t");
-  Serial.println(raw, BIN);
+  uint32_t mask = 0x80000000;
+  for (int i = 0; i < 32; i++)
+  {
+    if ((i > 0)  && (i % 4 == 0)) Serial.print(" ");
+    Serial.print((raw & mask) ? 1 : 0);
+    mask >>= 1;
+  }
+  Serial.println();
 
   float internal = tc.getInternal();
   Serial.print("internal:\t");
@@ -66,3 +79,6 @@ void loop()
   Serial.println(temp);
   delay(1000);
 }
+
+
+// -- END OF FILE --
